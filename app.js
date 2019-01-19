d7cd0eb95dd211073713e9c2a7121d540d0c9318
Sebastian Echeverri <sebastianecheverri900@gmail.com>
2019-01-19 15:03:23 -0500

    app.js

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-
-    if (req.query.nombre === null || req.query.nombre === ''){
-        res.send("<h1>Hola desconocido!</h1>")
-    } else {
-        res.send("<h1>Hola " + req.query.nombre + "!</h1>");
-    }
-  });
+  if (req.query.nombre === undefined)
+    var message = '<h1>Hola desconocido!</h1>'
+  else
+    var message = '<h1>Hola ' + req.query.nombre + '!</h1>'
+  res.send(message)
+});
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
