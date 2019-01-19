44debbbd228464a685f0bb61b54502cd2e5167f8
Sebastian Echeverri <sebastianecheverri900@gmail.com>
2019-01-19 14:44:12 -0500

    h1 added

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Hola " + req.query.nombre + "!");
+    res.send("<h1>Hola " + req.query.nombre + "!</h1>");
   });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
