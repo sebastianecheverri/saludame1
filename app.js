14ed29db60421c2a793dff986c8d8a5950d33b0f
Sebastian Echeverri <sebastianecheverri900@gmail.com>
2019-01-19 14:55:47 -0500

    undefined or null solved

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("<h1>Hola " + req.query.nombre + "!</h1>");
+
+    if (req.query.nombre === null || req.query.nombre === ''){
+        res.send("<h1>Hola desconocido!</h1>")
+    } else {
+        res.send("<h1>Hola " + req.query.nombre + "!</h1>");
+    }
   });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
